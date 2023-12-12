6be85e37b94cc2285b1d795cf23a0296f475d61e
Akankshadevloper <110593751+Akankshadevloper@users.noreply.github.com>
2023-12-12 17:25:59 +0530

    Add menu-driven functionality for managing books and students

--- src/Library.java
+++ src/Library.java
@@ -26,8 +26,79 @@ public class Library {
             ob.dispMenu();
             choice = input.nextInt();
 
+          switch (choice) {
 
+               
+                case 1:
+                    book b = new book();
+                    ob.addBook(b);
+                    break;
 
-  
+                
+                case 2:
+                    ob.upgradeBookQty();
+                    break;
 
+                
+                case 3:
+
+                    System.out.println(
+                            " press 1 to Search with Book Serial No.");
+                    System.out.println(
+                            " Press 2 to Search with Book's Author Name.");
+                    searchChoice = input.nextInt();
+
+                    
+                    switch (searchChoice) {
+
+                        
+                        case 1:
+                            ob.searchBySno();
+                            break;
+
+                        
+                        case 2:
+                            ob.searchByAuthorName();
+                    }
+                    break;
+
+                
+                case 4:
+                    ob.showAllBooks();
+                    break;
+
+                
+                case 5:
+                    student s = new student();
+                    obStudent.addStudent(s);
+                    break;
+
+                
+                case 6:
+                    obStudent.showAllStudents();
+                    break;
+
+                
+                case 7:
+                    obStudent.checkOutBook(ob);
+                    break;
+
+                
+                case 8:
+                    obStudent.checkInBook(ob);
+                    break;
+
+                
+                default:
+
+                    
+                    System.out.println("ENTER BETWEEN 0 TO 8.");
+            }
+
+        }
+
+        
+        while (choice != 0);
+    }
 }
+

50ce3a2ae071a6b300e9a0a1fa58663a60ebbe51
Akankshadevloper <110593751+Akankshadevloper@users.noreply.github.com>
2023-12-12 08:58:42 +0530

    Set up the main method and display menu in Library Management System
    Initialize the main method for the Library Management System.
     Creating a Scanner object for user input.
    Displaying a welcome message and menu options for user interaction.

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,18 @@
+import College.book;
+
+import java.util.Scanner;
+
 public class Library {
+
+  Scanner input = new Scanner(System.in);
+
+
+        System.out.println(
+                "********************Welcome to the Library Managment System ********************");
+        System.out.println(
+                "				 Select From The Following Options:			 ");
+        System.out.println(
+                "**********************************************************************");
+  
+
 }

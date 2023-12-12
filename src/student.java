8deda895640c363b621ab6cd8c56b1d11169c0f8
Akankshadevloper <110593751+Akankshadevloper@users.noreply.github.com>
2023-12-12 08:19:35 +0530

    Adding student class with basic information and book borrowing functionality

--- src/student.java
+++ src/student.java
@@ -1,2 +1,29 @@
+import College.book;
+import java.util.Scanner;
+
 public class student {
+
+  String studentName;
+  
+  String regNum;
+
+  book borrowedBook[] = new book[3];
+
+  public int booksCount = 0;
+
+  Scanner input = new Scanner(System.in);
+
+        public student()
+    {
+
+        System.out.println("Enter Student Name:");
+
+
+        this.studentName = input.nextLine();
+
+
+        System.out.println("Enter Registration Number:");
+        this.regNum = input.nextLine();
+    }
+
 }
